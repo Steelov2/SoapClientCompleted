e40974977685d9aa3d186b15a5d552eec60e08ce
Steelov <102536588+Steelov@users.noreply.github.com>
2022-10-31 00:02:41 +0600

    Completed/Clean without comments

--- src/main/java/com/example/demo/client/DocumentClient.java
+++ src/main/java/com/example/demo/client/DocumentClient.java
@@ -1,21 +1,15 @@
 package com.example.demo.client;
 
 import com.example.consumingwebservice.wsdl.StoreDocumentRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-import org.springframework.ws.soap.client.core.SoapActionCallback;
 import com.example.consumingwebservice.wsdl.StoreDocumentResponse;
 
-import java.util.Arrays;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class DocumentClient extends WebServiceGatewaySupport {
-    private static final Logger log = LoggerFactory.getLogger(DocumentClient.class);
     public StoreDocumentResponse getDocument(byte [] message) {
         StoreDocumentRequest request = new StoreDocumentRequest();
         request.setDocument(message);
-        log.info("Requesting location for " + Arrays.toString(message));
-
         StoreDocumentResponse response =(StoreDocumentResponse) getWebServiceTemplate()
                 .marshalSendAndReceive("http://localhost:8080/ws/document", request,
                         new SoapActionCallback(
